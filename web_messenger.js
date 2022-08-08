51ca270b09b296b5daa5ed98a66d2e1870190c0c
skallipo <79868440+skallipo@users.noreply.github.com>
2022-08-08 12:18:37 -0400

    Add files via upload

---
+++ web_messenger.js
@@ -0,0 +1,14 @@
+//genesys global function begin
+    (function (g, e, n, es, ys) {
+        g['_genesysJs'] = e;
+        g[e] = g[e] || function () {
+            (g[e].q = g[e].q || []).push(arguments)
+        };
+        g[e].t = 1 * new Date();
+        g[e].c = es;
+        ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+    })(window, 'Genesys', 'https://apps.mypurecloud.com/genesys-bootstrap/genesys.min.js', {
+        environment: 'use1',
+        deploymentId: '16c252a6-a8e4-4824-8d86-7508f9d7252d'
+    });
+//genesys global function end
